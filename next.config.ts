c01784912e6b71aed3ae0c4b7ebf4af6aeeb162c
Danjjinbibbang <hyjmex@gmail.com>
2025-05-04 02:33:11 +0900

    ssl 파일 누락 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 import fs from "fs";
+import path from "path";
+
+const sslDir = path.join(__dirname, "ssl");
+
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
@@ -7,8 +11,8 @@ const nextConfig: NextConfig = {
   },
   server: {
     https: {
-      key: fs.readFileSync("./ssl/localhost.key"),
-      cert: fs.readFileSync("./ssl/localhost.crt"),
+      key: fs.readFileSync(path.join(sslDir, "localhost.key")),
+      cert: fs.readFileSync(path.join(sslDir, "localhost.crt")),
     },
     port: 3001,
   },
